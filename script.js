b10de5481aa0e5eea1f3b15578b2c58337364b4b
lauraol <lauraoliveirape@gmail.com>
2020-04-10 22:53:18 -0300

    definindo os movimentos da cobrinha

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ snake[0] = {
     y:8 * box
 }
 
+let direction = "right"
+
 
 function criarBG(){
     context.fillStyle = "lightblue";
@@ -29,5 +31,31 @@ function criarCobrinha(){
 
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    //criando coordenadas de aumentar ou diminuir a cobrinha
+    if(direction == "rigth") snakeX += box;
+    if(direction == "left") snakeX -= box; //criando a ilusão que a cobrinha está indo para a esquerda
+    if(direction == "up") snakeY += box;
+    if(direction == "down") snakeY -=box;
+
+    snake.pop(); //tira o último elemento do array
+
+    let novaCabeca = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    //adiciona um elemento a frente
+    snake.unshift(novaCabeca);
+}
+
+//intervalo de 100 milisegundos para ir atualizando o joguinho
+let jogo = setInterval(iniciarJogo, 100);
+
+
